744e975b70c4d66d52443474e909be332b11db45
TomasM <tomas.bk148@gmail.com>
2024-08-25 14:54:10 +0200

    method for calculating md5

--- FolderSynchronizer.py
+++ FolderSynchronizer.py
@@ -1,4 +1,5 @@
 import logging
+import hashlib
 
 class FolderSynchronizer:
     def __init__(self, source, replica, log_file):
@@ -12,3 +13,8 @@ class FolderSynchronizer:
         console = logging.StreamHandler()
         console.setLevel(logging.INFO)
         logging.getLogger().addHandler(console)
+
+    def calculate_md5(self, file_path):
+        """Calculate the MD5 checksum of a file."""
+        with open(file_path, "rb") as f:
+            return hashlib.md5(f.read()).hexdigest()
