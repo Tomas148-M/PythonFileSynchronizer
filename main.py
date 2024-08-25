29a7512c9a864108e4c864179bd74a53857b674f
TomasM <tomas.bk148@gmail.com>
2024-08-25 14:39:27 +0200

    Create main.py
    Add class for parsing of input arguments

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+
+class FolderSynchronizer:
+    def __init__(self):
+        self.args = self.parse_arguments()
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description="Synchronize two folders.")
+        parser.add_argument("--source", required=True, help="Path to the source folder")
+        parser.add_argument("--replica", required=True, help="Path to the replica folder")
+        parser.add_argument("--interval", type=int, required=True, help="Synchronization interval in seconds")
+        parser.add_argument("--logfile", required=True, help="Path to the log file")
+        return parser.parse_args()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = FolderSynchronizer()
